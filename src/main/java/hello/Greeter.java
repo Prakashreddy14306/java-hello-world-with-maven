8ba4bb60201c8d76ec1fc33f9df0bc33e800f14a
devopskroy <109326965+devopskroy@users.noreply.github.com>
2023-07-31 09:46:20 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "DevOps Project from Kishan roy for CLOUD-23 !!!!";
+        return "DevOps Project from Kishan roy for CLOUD-24 !!!!";
     }
 }
